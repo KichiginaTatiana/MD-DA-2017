68e004205b71d12dfda700ac6b9f43f715620d23
strstr <strifika@mail.ru>
2017-10-01 22:07:26 +0500

    classwork1 25.09.17

---
+++ Tatiana/classwork1.R
@@ -0,0 +1,20 @@
+35 + 777
+13 * 47
+675 * 15
+849 * 629
+
+11111111*11111111
+
+x <-c(3,7,12,15, 20)
+y <-c(2,5,8,11,15)
+x*y
+
+plot(x, y)
+
+learn <-c("math" = 40, "english"= 40,"pe" = 10, "progr" = 150)
+plot(learn)
+pie(learn)
+barplot(learn)
+
+drinks <-rnorm(10000, mean = 450, sd = 4)
+length(which(drinks > 455))/10000
